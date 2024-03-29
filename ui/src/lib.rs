71d7740f7dce719d54ec05207c2d272ba80eb7f7
Jon Saw <jon.saw@gmail.com>
2024-03-29 11:38:19 +0800

    Add Style trait and cn! macro to handle CSS styling

--- ui/src/lib.rs
+++ ui/src/lib.rs
@@ -1,2 +1,39 @@
 pub mod components;
 pub mod custom_animated_show;
+
+pub trait Style {
+    fn style(&self) -> String;
+}
+
+impl Style for String {
+    fn style(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl Style for Option<String> {
+    fn style(&self) -> String {
+        self.as_ref().map(|s| s.style()).unwrap_or_default()
+    }
+}
+
+impl Style for &str {
+    fn style(&self) -> String {
+        self.to_string()
+    }
+}
+
+#[macro_export]
+macro_rules! cn {
+    ($($styles: expr),*) => {
+        {
+            use $crate::Style;
+            let mut result = String::new();
+            $(
+                result.push_str(" ");
+                result.push_str(&$styles.style());
+            )*
+            result
+        }
+    };
+}
