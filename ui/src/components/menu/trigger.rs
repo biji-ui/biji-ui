e8a54df44aacdeb2c358a50073c7a3a13862e351
Jon Saw <jon.saw@gmail.com>
2024-04-03 16:36:30 +0800

    Satisfy unused variable warning

--- ui/src/components/menu/trigger.rs
+++ ui/src/components/menu/trigger.rs
@@ -15,10 +15,10 @@ where
 #[component]
 pub fn Trigger(#[prop(into, optional)] class: String, children: Children) -> impl IntoView {
     let ctx = expect_context::<MenuContext>();
-    let trigger_ref = ctx.trigger_ref;
+    let _trigger_ref = ctx.trigger_ref;
 
     view! {
-        <button _ref={trigger_ref} class={class}>
+        <button _ref={_trigger_ref} class={class}>
             {children()}
         </button>
     }
