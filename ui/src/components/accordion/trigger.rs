016ebadad98f77f3aeb74d78f41990f962f16b6f
Jon Saw <jon.saw@gmail.com>
2024-04-04 11:10:27 +0800

    Update accordion trigger to include data-highlighted attribute

--- ui/src/components/accordion/trigger.rs
+++ ui/src/components/accordion/trigger.rs
@@ -1,9 +1,12 @@
 use leptos::*;
 
-use crate::components::accordion::{events::ItemEvents, item::AccordionItemContext};
+use crate::components::accordion::{
+    events::ItemEvents, item::AccordionItemContext, root::AccordionContext,
+};
 
 #[component]
 pub fn Trigger(#[prop(into, optional)] class: String, children: Children) -> impl IntoView {
+    let root_ctx = expect_context::<AccordionContext>();
     let ctx = expect_context::<AccordionItemContext>();
     let trigger_ref = ctx.trigger_ref;
 
@@ -14,6 +17,9 @@ pub fn Trigger(#[prop(into, optional)] class: String, children: Children) -> imp
                 class={class}
                 data-state={move || if ctx.open.get() { "open" } else { "closed" }}
                 data-value={ctx.index}
+                data-highlighted={move || {
+                    root_ctx.current_focus.get().map(|f| f == ctx.index).unwrap_or(false)
+                }}
             >
 
                 {children()}
