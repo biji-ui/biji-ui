fdbda856961ed44dc008b6671597ab813f271cb3
Jon Saw <jon.saw@gmail.com>
2024-04-04 22:06:41 +0800

    Added chevron right icon in documentation

---
+++ app/src/icons/chevron_right.rs
@@ -0,0 +1,21 @@
+use leptos::*;
+
+#[component]
+pub fn ChevronRight(#[prop(into, optional)] class: String) -> impl IntoView {
+    view! {
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class={class}
+        >
+            <path d="m9 18 6-6-6-6"></path>
+        </svg>
+    }
+}
--- app/src/icons/mod.rs
+++ app/src/icons/mod.rs
@@ -1,5 +1,7 @@
 pub mod caret;
+pub mod chevron_right;
 pub mod ellipsis;
 
 pub use caret::Caret;
+pub use chevron_right::ChevronRight;
 pub use ellipsis::Ellipsis;
