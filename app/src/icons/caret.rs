f3eb6751c78f9b350da53dcde8a6d52c3d8df4d2
Jon Saw <jon.saw@gmail.com>
2024-04-12 08:23:26 +0800

    Remove defined height and width

--- app/src/icons/caret.rs
+++ app/src/icons/caret.rs
@@ -5,8 +5,6 @@ pub fn Caret(#[prop(into, optional)] class: String) -> impl IntoView {
     view! {
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
             fill="currentColor"
             viewBox="0 0 256 256"
             class={class}
--- app/src/icons/chevron_right.rs
+++ app/src/icons/chevron_right.rs
@@ -5,8 +5,6 @@ pub fn ChevronRight(#[prop(into, optional)] class: String) -> impl IntoView {
     view! {
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
