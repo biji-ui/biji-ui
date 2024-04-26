8a64d388995db987e5ff86cdb688226c5af8b1c4
Jon Saw <jon.saw@gmail.com>
2024-04-26 10:43:33 +0800

    Remove indexes from doc menubar example

--- app/src/pages/docs/menubar.rs
+++ app/src/pages/docs/menubar.rs
@@ -16,110 +16,96 @@ pub fn MenubarDocPage() -> impl IntoView {
 pub fn MenubarExample() -> impl IntoView {
     use biji_ui::components::menubar;
 
-    const ITEM_STYLE: &str = "flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 hover:bg-accent hover:text-accent-foreground !ring-0 !ring-transparent data-[highlighted]:bg-muted";
+    const ITEM_STYLE: &str = "flex items-center py-1.5 px-2 text-sm rounded-sm cursor-pointer outline-none select-none focus:outline-none hover:bg-accent hover:text-accent-foreground !ring-0 !ring-transparent data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[highlighted]:bg-muted";
 
     view! {
         <menubar::Root class="flex">
-            <menubar::Menu index=0 class="relative">
-                <menubar::MenuTrigger class="flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 hover:bg-accent hover:text-accent-foreground !ring-0 !ring-transparent data-[highlighted]:bg-muted">
+            <menubar::Menu class="relative">
+                <menubar::MenuTrigger class="flex items-center py-1.5 px-2 text-sm rounded-sm cursor-pointer outline-none select-none focus:outline-none hover:bg-accent hover:text-accent-foreground !ring-0 !ring-transparent data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[highlighted]:bg-muted">
                     "File"
                 </menubar::MenuTrigger>
                 <menubar::MenuContent
-                    class="absolute flex w-56 min-w-[8rem] flex-col rounded-md border bg-popover p-1 text-popover-foreground shadow-md focus:outline-none"
+                    class="flex absolute flex-col p-1 w-56 rounded-md border shadow-md min-w-[8rem] bg-popover text-popover-foreground focus:outline-none"
                     show_class="z-10 opacity-100 transition duration-150 ease-in"
-                    hide_class="-z-10 opacity-0 transition duration-200 ease-out"
+                    hide_class="opacity-0 transition duration-200 ease-out -z-10"
                     hide_delay={Duration::from_millis(200)}
                 >
-                    <menubar::Item index=0 class={ITEM_STYLE}>
-                        <button class="flex h-full w-full" on:click={|_| log!("New file clicked")}>
+                    <menubar::Item class={ITEM_STYLE}>
+                        <button class="flex w-full h-full" on:click={|_| log!("New file clicked")}>
                             "New file..."
                         </button>
                     </menubar::Item>
-                    <menubar::Item index=1 class={ITEM_STYLE} disabled=true>
+                    <menubar::Item class={ITEM_STYLE} disabled=true>
                         "New window"
                     </menubar::Item>
                     <hr class="h-px bg-gray-100"/>
-                    <menubar::SubMenuItem index=2 class="relative">
+                    <menubar::SubMenuItem class="relative">
                         <menubar::SubMenuItemTrigger class={cn!(
                             "h-full w-full items-center justify-between", ITEM_STYLE
                         )}>
                             "Share" <icons::ChevronRight class="h-4"></icons::ChevronRight>
                         </menubar::SubMenuItemTrigger>
                         <menubar::SubMenuItemContent
-                            class="absolute flex w-56 min-w-[8rem] flex-col rounded-md border bg-popover p-1 text-popover-foreground shadow-md left-full top-0 focus:outline-none"
+                            class="flex absolute top-0 left-full flex-col p-1 w-56 rounded-md border shadow-md min-w-[8rem] bg-popover text-popover-foreground focus:outline-none"
                             show_class="z-10 opacity-100 transition duration-150 ease-in"
-                            hide_class="-z-10 opacity-0 transition duration-200 ease-out"
+                            hide_class="opacity-0 transition duration-200 ease-out -z-10"
                             hide_delay={Duration::from_millis(200)}
                         >
-                            <menubar::Item index=0 class={ITEM_STYLE}>
-                                "Email"
-                            </menubar::Item>
-                            <menubar::Item index=1 class={ITEM_STYLE} disabled=true>
+                            <menubar::Item class={ITEM_STYLE}>"Email"</menubar::Item>
+                            <menubar::Item class={ITEM_STYLE} disabled=true>
                                 "Message"
                             </menubar::Item>
-                            <menubar::Item index=2 class={ITEM_STYLE}>
-                                "Copy link"
-                            </menubar::Item>
+                            <menubar::Item class={ITEM_STYLE}>"Copy link"</menubar::Item>
                             <hr class="h-px bg-gray-100"/>
-                            <menubar::SubMenuItem index=3 class="relative">
+                            <menubar::SubMenuItem class="relative">
                                 <menubar::SubMenuItemTrigger class={cn!(
                                     "h-full w-full items-center justify-between", ITEM_STYLE
                                 )}>
                                     "More" <icons::ChevronRight class="h-4"></icons::ChevronRight>
                                 </menubar::SubMenuItemTrigger>
                                 <menubar::SubMenuItemContent
-                                    class="absolute flex w-56 min-w-[8rem] flex-col rounded-md border bg-popover p-1 text-popover-foreground shadow-md left-full top-0 focus:outline-none"
+                                    class="flex absolute top-0 left-full flex-col p-1 w-56 rounded-md border shadow-md min-w-[8rem] bg-popover text-popover-foreground focus:outline-none"
                                     show_class="z-10 opacity-100 transition duration-150 ease-in"
-                                    hide_class="-z-10 opacity-0 transition duration-200 ease-out"
+                                    hide_class="opacity-0 transition duration-200 ease-out -z-10"
                                     hide_delay={Duration::from_millis(200)}
                                 >
-                                    <menubar::Item index=0 class={ITEM_STYLE}>
-                                        "Facebook"
-                                    </menubar::Item>
-                                    <menubar::Item index=1 class={ITEM_STYLE}>
-                                        "LinkedIn"
-                                    </menubar::Item>
-                                    <menubar::Item index=2 class={ITEM_STYLE}>
-                                        "Instagram"
-                                    </menubar::Item>
+                                    <menubar::Item class={ITEM_STYLE}>"Facebook"</menubar::Item>
+                                    <menubar::Item class={ITEM_STYLE}>"LinkedIn"</menubar::Item>
+                                    <menubar::Item class={ITEM_STYLE}>"Instagram"</menubar::Item>
                                 </menubar::SubMenuItemContent>
                             </menubar::SubMenuItem>
                         </menubar::SubMenuItemContent>
                     </menubar::SubMenuItem>
                 </menubar::MenuContent>
             </menubar::Menu>
-            <menubar::Menu index=1 class="relative">
+            <menubar::Menu class="relative">
                 <menubar::MenuTrigger class="flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 hover:bg-accent hover:text-accent-foreground !ring-0 !ring-transparent data-[highlighted]:bg-muted">
                     "Edit"
                 </menubar::MenuTrigger>
                 <menubar::MenuContent
-                    class="absolute flex w-56 min-w-[8rem] flex-col rounded-md border bg-popover p-1 text-popover-foreground shadow-md focus:outline-none"
+                    class="flex absolute flex-col p-1 w-56 rounded-md border shadow-md min-w-[8rem] bg-popover text-popover-foreground focus:outline-none"
                     show_class="z-10 opacity-100 transition duration-150 ease-in"
-                    hide_class="-z-10 opacity-0 transition duration-200 ease-out"
+                    hide_class="opacity-0 transition duration-200 ease-out -z-10"
                     hide_delay={Duration::from_millis(200)}
                 >
-                    <menubar::Item index=0 class={ITEM_STYLE}>
-                        "Undo"
-                    </menubar::Item>
-                    <menubar::Item index=1 class={ITEM_STYLE}>
-                        "Redo"
-                    </menubar::Item>
+                    <menubar::Item class={ITEM_STYLE}>"Undo"</menubar::Item>
+                    <menubar::Item class={ITEM_STYLE}>"Redo"</menubar::Item>
                 </menubar::MenuContent>
             </menubar::Menu>
-            <menubar::Menu index=2 class="relative">
+            <menubar::Menu class="relative">
                 <menubar::MenuTrigger class="flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 hover:bg-accent hover:text-accent-foreground !ring-0 !ring-transparent data-[highlighted]:bg-muted">
                     "Components"
                 </menubar::MenuTrigger>
                 <menubar::MenuContent
-                    class="absolute flex w-56 min-w-[8rem] flex-col rounded-md border bg-popover p-1 text-popover-foreground shadow-md focus:outline-none"
+                    class="flex absolute flex-col p-1 w-56 rounded-md border shadow-md min-w-[8rem] bg-popover text-popover-foreground focus:outline-none"
                     show_class="z-10 opacity-100 transition duration-150 ease-in"
-                    hide_class="-z-10 opacity-0 transition duration-200 ease-out"
+                    hide_class="opacity-0 transition duration-200 ease-out -z-10"
                     hide_delay={Duration::from_millis(200)}
                 >
-                    <menubar::Item index=0 class={ITEM_STYLE}>
+                    <menubar::Item class={ITEM_STYLE}>
                         <a href="/docs/accordion">"Accordion"</a>
                     </menubar::Item>
-                    <menubar::Item index=1 class={ITEM_STYLE}>
+                    <menubar::Item class={ITEM_STYLE}>
                         <a href="/docs/dropdown-menu">"Dropdown Menu"</a>
                     </menubar::Item>
                 </menubar::MenuContent>
