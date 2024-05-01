546ca56e19fb0b383b5120616362a26df09f5a74
Jon Saw <jon.saw@gmail.com>
2024-05-01 21:59:20 +0800

    Added x icon

--- app/src/icons/mod.rs
+++ app/src/icons/mod.rs
@@ -2,8 +2,10 @@ pub mod align_justify;
 pub mod caret;
 pub mod chevron_right;
 pub mod ellipsis;
+pub mod x;
 
 pub use align_justify::AlignJustify;
 pub use caret::Caret;
 pub use chevron_right::ChevronRight;
 pub use ellipsis::Ellipsis;
+pub use x::X;
