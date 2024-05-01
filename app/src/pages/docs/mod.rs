7b02c24d71b62e2657fcebd91712cb30ae57866d
Jon Saw <jon.saw@gmail.com>
2024-05-01 22:16:10 +0800

    Sidebar for xs, s and md screeens

--- app/src/pages/docs/mod.rs
+++ app/src/pages/docs/mod.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use leptos::*;
 use leptos_router::*;
 
@@ -7,8 +9,116 @@ pub mod dropdown_menu;
 pub mod getting_started;
 pub mod menubar;
 
+use biji_ui::components::dialog::{self as dialogui, context::DialogContext};
+use leptos_use::use_media_query;
+
+use crate::icons;
+
 #[component]
-pub fn DocsPage() -> impl IntoView {
+pub fn TopNav() -> impl IntoView {
+    view! {
+        <div
+            class="fixed inset-x-0 top-0 z-10 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80 backdrop-blur-sm lg:left-72 xl:left-80 dark:backdrop-blur bg-white/[var(--bg-opacity-light)] dark:bg-zinc-900/[var(--bg-opacity-dark)]"
+            style="--bg-opacity-light: 0.5; --bg-opacity-dark: 0.2;"
+        >
+            <div class="absolute inset-x-0 top-full h-px transition bg-zinc-900/10 dark:bg-white/10"></div>
+            <div class="hidden lg:block lg:max-w-md lg:flex-auto"></div>
+            <div class="flex items-center gap-5 lg:hidden">
+                <Sidebar/>
+                <a aria-label="Home" href="/">
+                    "biji"
+                </a>
+            </div>
+            <div class="flex items-center gap-5">
+                <nav class="hidden md:block">
+                    <ul role="list" class="flex items-center gap-8">
+                        <li>
+                            <a
+                                class="text-sm leading-5 text-zinc-600 transition dark:text-zinc-400 dark:hover:text-white hover:text-zinc-900"
+                                href="https://docs.rs"
+                            >
+                                "API"
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                class="text-sm leading-5 text-zinc-600 transition dark:text-zinc-400 dark:hover:text-white hover:text-zinc-900"
+                                href="/"
+                            >
+                                "Documentation"
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                class="text-sm leading-5 text-zinc-600 transition dark:text-zinc-400 dark:hover:text-white hover:text-zinc-900"
+                                href="https://github.com"
+                            >
+                                "Support"
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    }
+}
+
+#[component]
+pub fn SidebarTrigger() -> impl IntoView {
+    let ctx = expect_context::<DialogContext>();
+
+    let is_large_screen = use_media_query("(min-width: 1024px)");
+
+    create_effect(move |_| {
+        if is_large_screen.get() {
+            ctx.close();
+        }
+    });
+
+    view! {
+        <Show
+            when={move || !ctx.open.get()}
+            fallback={|| {
+                view! { <icons::X class="w-2.5 stroke-zinc-900 dark:stroke-white"></icons::X> }
+            }}
+        >
+
+            <icons::AlignJustify class="w-2.5 stroke-zinc-900 dark:stroke-white"></icons::AlignJustify>
+        </Show>
+    }
+}
+
+#[component]
+pub fn Sidebar() -> impl IntoView {
+    view! {
+        <dialogui::Root>
+            <dialogui::Trigger class="flex h-6 w-6 items-center justify-center rounded-md transition dark:hover:bg-white/5 hover:bg-zinc-900/5">
+                <SidebarTrigger/>
+            </dialogui::Trigger>
+            <Portal>
+                <dialogui::Overlay
+                    class="transition-opacity ease-linear duration-300 fixed inset-0 top-14 bg-zinc-400/20 backdrop-blur-sm dark:bg-black/40"
+                    show_class="opacity-100"
+                    hide_class="opacity-0"
+                    hide_delay={Duration::from_millis(300)}
+                ></dialogui::Overlay>
+                <dialogui::Content
+                    class="transition ease-in-out duration-300 fixed bottom-0 left-0 top-14 w-full overflow-y-auto bg-white px-4 pb-4 pt-6 shadow-lg shadow-zinc-900/10 ring-1 ring-zinc-900/10 min-[416px]:max-w-sm sm:px-6 sm:pb-10 dark:bg-zinc-900 dark:ring-zinc-800"
+                    show_class="translate-x-0"
+                    hide_class="-translate-x-full"
+                    hide_delay={Duration::from_millis(300)}
+                >
+                    <div>
+                        <SidebarNav/>
+                    </div>
+                </dialogui::Content>
+            </Portal>
+        </dialogui::Root>
+    }
+}
+
+#[component]
+pub fn SidebarNav(#[prop(into, optional)] class: String) -> impl IntoView {
     let location = use_location();
 
     let introduction = [("/docs/getting-started", "Getting started")];
@@ -20,109 +130,93 @@ pub fn DocsPage() -> impl IntoView {
         ("/docs/menubar", "Menubar"),
     ];
 
+    let dialog_ctx = use_context::<DialogContext>();
+
+    view! {
+        <nav class={class}>
+            <ul role="list">
+                <li class="relative mt-6 md:mt-0">
+                    <h2 class="text-xs font-semibold text-zinc-900 dark:text-white">
+                        "Introduction"
+                    </h2>
+                    <ul class="border-l border-transparent">
+                        {introduction
+                            .into_iter()
+                            .map(|(path, title)| {
+                                view! {
+                                    <li class="relative">
+                                        <a
+                                            href={path}
+                                            class="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-600 dark:text-zinc-400 dark:hover:text-white hover:text-zinc-900"
+                                            class:font-medium={move || {
+                                                location.pathname.get() == path
+                                            }}
+
+                                            on:click={move |_| {
+                                                if let Some(ctx) = dialog_ctx {
+                                                    ctx.close();
+                                                }
+                                            }}
+                                        >
+
+                                            {title}
+                                        </a>
+                                    </li>
+                                }
+                            })
+                            .collect::<Vec<_>>()}
+                    </ul>
+                </li>
+                <li class="relative mt-6">
+                    <h2 class="text-xs font-semibold text-zinc-900 dark:text-white">
+                        "Components"
+                    </h2>
+                    <ul class="border-l border-transparent">
+                        {components
+                            .into_iter()
+                            .map(|(path, title)| {
+                                view! {
+                                    <li class="relative">
+                                        <a
+                                            href={path}
+                                            class="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-600 dark:text-zinc-400 dark:hover:text-white hover:text-zinc-900"
+                                            class:font-medium={move || {
+                                                location.pathname.get() == path
+                                            }}
+
+                                            on:click={move |_| {
+                                                if let Some(ctx) = dialog_ctx {
+                                                    ctx.close();
+                                                }
+                                            }}
+                                        >
+
+                                            {title}
+                                        </a>
+                                    </li>
+                                }
+                            })
+                            .collect::<Vec<_>>()}
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    }
+}
+
+#[component]
+pub fn DocsPage() -> impl IntoView {
     view! {
         <div class="h-full lg:ml-72 xl:ml-80">
             <header class="contents lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex">
                 <div class="contents lg:pointer-events-auto lg:block lg:w-72 lg:overflow-y-auto lg:border-r lg:border-zinc-900/10 lg:px-6 lg:pb-8 lg:pt-4 lg:dark:border-white/10 xl:w-80">
                     <div class="hidden lg:flex">
                         <a aria-label="Home" href="/">
-                            "Biji UI"
+                            "biji"
                         </a>
                     </div>
-                    <div
-                        class="fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80 backdrop-blur-sm lg:left-72 xl:left-80 dark:backdrop-blur bg-white/[var(--bg-opacity-light)] dark:bg-zinc-900/[var(--bg-opacity-dark)]"
-                        style="--bg-opacity-light: 0.5; --bg-opacity-dark: 0.2;"
-                    >
-                        <div class="absolute inset-x-0 top-full h-px transition bg-zinc-900/10 dark:bg-white/10"></div>
-                        <div class=""></div>
-                        <div class="flex items-center gap-5">
-                            <nav class="hidden md:block">
-                                <ul role="list" class="flex items-center gap-8">
-                                    <li>
-                                        <a
-                                            class="text-sm leading-5 text-zinc-600 transition dark:text-zinc-400 dark:hover:text-white hover:text-zinc-900"
-                                            href="https://docs.rs"
-                                        >
-                                            "API"
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a
-                                            class="text-sm leading-5 text-zinc-600 transition dark:text-zinc-400 dark:hover:text-white hover:text-zinc-900"
-                                            href="/"
-                                        >
-                                            "Documentation"
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a
-                                            class="text-sm leading-5 text-zinc-600 transition dark:text-zinc-400 dark:hover:text-white hover:text-zinc-900"
-                                            href="https://github.com"
-                                        >
-                                            "Support"
-                                        </a>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
-
-                    <nav class="hidden lg:mt-10 lg:block">
-                        <ul role="list">
-                            <li class="relative mt-6 md:mt-0">
-                                <h2 class="text-xs font-semibold text-zinc-900 dark:text-white">
-                                    "Introduction"
-                                </h2>
-                                <ul class="border-l border-transparent">
-                                    {introduction
-                                        .into_iter()
-                                        .map(|(path, title)| {
-                                            view! {
-                                                <li class="relative">
-                                                    <a
-                                                        href={path}
-                                                        class="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-600 dark:text-zinc-400 dark:hover:text-white hover:text-zinc-900"
-                                                        class:font-medium={move || {
-                                                            location.pathname.get() == path
-                                                        }}
-                                                    >
-
-                                                        {title}
-                                                    </a>
-                                                </li>
-                                            }
-                                        })
-                                        .collect::<Vec<_>>()}
-                                </ul>
-                            </li>
-                            <li class="relative mt-6">
-                                <h2 class="text-xs font-semibold text-zinc-900 dark:text-white">
-                                    "Components"
-                                </h2>
-                                <ul class="border-l border-transparent">
-                                    {components
-                                        .into_iter()
-                                        .map(|(path, title)| {
-                                            view! {
-                                                <li class="relative">
-                                                    <a
-                                                        href={path}
-                                                        class="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-600 dark:text-zinc-400 dark:hover:text-white hover:text-zinc-900"
-                                                        class:font-medium={move || {
-                                                            location.pathname.get() == path
-                                                        }}
-                                                    >
-
-                                                        {title}
-                                                    </a>
-                                                </li>
-                                            }
-                                        })
-                                        .collect::<Vec<_>>()}
-                                </ul>
-                            </li>
-                        </ul>
-                    </nav>
+                    <TopNav/>
+                    <SidebarNav class="hidden lg:mt-10 lg:block"/>
                 </div>
             </header>
             <div class="relative flex h-full flex-col px-4 pt-14 sm:px-6 lg:px-8">
