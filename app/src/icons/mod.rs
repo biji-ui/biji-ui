3e04ab1333b808aa95fc51486edbf685b612b1bf
Jon Saw <jon.saw@gmail.com>
2024-05-01 21:59:12 +0800

    Added align justify icon

--- app/src/icons/mod.rs
+++ app/src/icons/mod.rs
@@ -1,7 +1,9 @@
+pub mod align_justify;
 pub mod caret;
 pub mod chevron_right;
 pub mod ellipsis;
 
+pub use align_justify::AlignJustify;
 pub use caret::Caret;
 pub use chevron_right::ChevronRight;
 pub use ellipsis::Ellipsis;
