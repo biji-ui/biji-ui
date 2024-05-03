cb19d804faaf5403b63addd8685d8bd5f87c264a
Jon Saw <jon.saw@gmail.com>
2024-05-03 23:15:03 +0800

    Added moon icon

--- app/src/icons/mod.rs
+++ app/src/icons/mod.rs
@@ -3,6 +3,7 @@ pub mod caret;
 pub mod check;
 pub mod chevron_right;
 pub mod ellipsis;
+pub mod moon;
 pub mod x;
 
 pub use align_justify::AlignJustify;
@@ -10,4 +11,5 @@ pub use caret::Caret;
 pub use check::Check;
 pub use chevron_right::ChevronRight;
 pub use ellipsis::Ellipsis;
+pub use moon::Moon;
 pub use x::X;
