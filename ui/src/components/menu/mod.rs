340e00d303694b0944f0d43e5a47e008e77488bb
Jon Saw <jon.saw@gmail.com>
2025-10-03 17:22:50 +0800

    Add positioning prop to Menu and Menubar components

--- ui/src/components/menu/mod.rs
+++ ui/src/components/menu/mod.rs
@@ -7,12 +7,14 @@ pub use crate::components::menubar::item::SubMenuItemContent as SubMenuContent;
 pub use crate::components::menubar::item::SubMenuItemTrigger as SubMenuTrigger;
 pub use crate::components::menubar::menu::MenuContent as Content;
 pub use crate::components::menubar::menu::MenuTrigger as Trigger;
+pub use crate::utils::positioning::Positioning;
 
 #[component]
 pub fn Menu(
     #[prop(default = false)] disabled: bool,
     #[prop(into, optional)] class: String,
     #[prop(default = false)] allow_loop: bool,
+    #[prop(default = Positioning::BottomStart)] positioning: Positioning,
     children: Children,
 ) -> impl IntoView {
     let ctx = RootContext {
@@ -23,7 +25,7 @@ pub fn Menu(
     provide_context(ctx);
 
     view! {
-        <MenuComponent disabled={disabled} class={class}>
+        <MenuComponent disabled={disabled} class={class} positioning={positioning}>
             {children()}
         </MenuComponent>
     }
