de43792190f57021acb839885f8256151eb22609
Jon Saw <jon.saw@gmail.com>
2025-10-06 13:30:23 +0800

    Switch to Rollup, update Shiki, and refactor build setup
    - Replace esbuild with Rollup for bundling - Update Shiki and related
    dependencies to v3.x - Remove old build and highlight scripts - Add
    Rollup config and new shiki script - Update Rust bindings to use new
    bundle path - Update .gitignore for new build artifacts

--- jspackages/src/highlight.rs
+++
@@ -1,9 +0,0 @@
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen(module = "/output/highlight.js")]
-extern "C" {
-    pub fn highlight(code: &str, language: &str) -> String;
-
-    #[wasm_bindgen(js_name = "highlightElement")]
-    pub fn highlight_element(element: &web_sys::HtmlElement);
-}
