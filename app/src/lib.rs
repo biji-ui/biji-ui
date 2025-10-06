e11bb90eaaf279b6e8e55f4ed36dd077f59196a7
Jon Saw <jon.saw@gmail.com>
2025-10-06 12:23:38 +0800

    Use Leptos' built-in tailwindcss support

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -23,8 +23,9 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
             <head>
                 <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <AutoReload options=options.clone() />
+                <AutoReload options={options.clone()} />
                 <HydrationScripts options />
+                <link rel="stylesheet" id="leptos" href="/pkg/biji-ui.css" />
                 <MetaTags />
             </head>
             <body>
@@ -40,27 +41,30 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-        <Stylesheet id="leptos" href="/pkg/biji-ui.css"/>
+        <Stylesheet id="leptos" href="/pkg/biji-ui.css" />
 
         // sets the document title
-        <Title text="Biji UI"/>
+        <Title text="Biji UI" />
 
         // content for this welcome page
         <Router>
             <div class="w-full">
-                <Routes  fallback={|| {
+                <Routes fallback={|| {
                     let mut outside_errors = Errors::default();
                     outside_errors.insert_with_default_key(AppError::NotFound);
-                    view! { <ErrorTemplate outside_errors/> }.into_view()
+                    view! { <ErrorTemplate outside_errors /> }.into_view()
                 }}>
-                    <Route path=path!("/") view={|| view! { <Redirect path="/docs/getting-started"/> }}/>
-                    <ParentRoute path=path!("/docs") view={DocsPage}>
-                        <Route path=path!("getting-started") view={GettingStartedPage}/>
-                        <Route path=path!("accordion") view={AccordionDocPage}/>
-                        <Route path=path!("dialog") view={DialogDocPage}/>
-                        <Route path=path!("dropdown-menu") view={DropdownMenuDocPage}/>
-                        <Route path=path!("menubar") view={MenubarDocPage}/>
-                        <Route path=path!("tooltip") view={TooltipDocPage}/>
+                    <Route
+                        path={path!("/")}
+                        view={|| view! { <Redirect path="/docs/getting-started" /> }}
+                    />
+                    <ParentRoute path={path!("/docs")} view={DocsPage}>
+                        <Route path={path!("getting-started")} view={GettingStartedPage} />
+                        <Route path={path!("accordion")} view={AccordionDocPage} />
+                        <Route path={path!("dialog")} view={DialogDocPage} />
+                        <Route path={path!("dropdown-menu")} view={DropdownMenuDocPage} />
+                        <Route path={path!("menubar")} view={MenubarDocPage} />
+                        <Route path={path!("tooltip")} view={TooltipDocPage} />
                     </ParentRoute>
                 </Routes>
             </div>
