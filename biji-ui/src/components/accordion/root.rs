700f19e8f249026f4911d9c527f7299f01f7e641
Jon Saw <jon.saw@gmail.com>
2026-03-19 22:37:37 +0800

    Refactor Accordion context to AccordionState and add use_accordion hook

--- biji-ui/src/components/accordion/root.rs
+++ biji-ui/src/components/accordion/root.rs
@@ -1,10 +1,14 @@
 use leptos::{context::Provider, prelude::*};
 
-use super::context::{AccordionContext, RootContext};
+use super::context::{AccordionState, RootContext};
+
+pub fn use_accordion() -> AccordionState {
+    expect_context::<AccordionState>()
+}
 
 #[component]
-pub fn Root(
-    children: Children,
+pub fn RootWith<IV: IntoView + 'static>(
+    children: impl Fn(AccordionState) -> IV + Send + Sync + 'static,
     #[prop(into, optional)] class: String,
     #[prop(default = false)] allow_loop: bool,
 ) -> impl IntoView {
@@ -13,16 +17,29 @@ pub fn Root(
         allow_loop,
         ..RootContext::default()
     };
-    let ctx = AccordionContext {
+    let state = AccordionState {
         root: RwSignal::new(root_ctx),
         accordion_ref,
     };
 
     view! {
-        <Provider value={ctx}>
+        <Provider value={state}>
             <div node_ref={accordion_ref} class={class}>
-                <Provider value={root_ctx}>{children()}</Provider>
+                <Provider value={root_ctx}>{children(state)}</Provider>
             </div>
         </Provider>
+    }
+}
+
+#[component]
+pub fn Root(
+    children: ChildrenFn,
+    #[prop(into, optional)] class: String,
+    #[prop(default = false)] allow_loop: bool,
+) -> impl IntoView {
+    view! {
+        <RootWith allow_loop={allow_loop} class={class} let:_>
+            {children()}
+        </RootWith>
     }
 }
